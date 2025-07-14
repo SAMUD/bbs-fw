5cf66475c5e3e0b843f3aedf2819b1e7a2fb22ec
Elvin Luff <elvin@eelviny.me>
2025-07-14 21:40:12 +0200

    feat: WIP programmer validation logic

--- src/programmer/main.py
+++ src/programmer/main.py
@@ -1,5 +1,42 @@
+import yaml
+import argparse
+from pint import UnitRegistry
+
+def read_yaml(file_path):
+    with open(file_path, "r") as file:
+        return yaml.safe_load(file)
+
+
+def format_header_define(key, value):
+    return f"#define {key} {value}"
+
+def recurse_config_key(config, begin_key=""):
+	for key, value in config.items():
+		if isinstance(value, dict):
+			recurse_config_key(value, f"{begin_key}_{key}")
+		elif isinstance(value, list):
+			for item in value:
+				if isinstance(item, dict):
+					recurse_config_key(item, f"{begin_key}_{key}")
+		else:
+			print(format_header_define(f"{begin_key}_{key}".upper(), value))
+
 def main():
-    print("Hello from bbsxtra-programmer!")
+    parser = argparse.ArgumentParser(
+        prog="bbsxtra-programmer",
+        description="A tool to compile firmware for the Bafang BBS02/BBSHD ebike motor",
+    )
+    parser.add_argument("filename", help="Path to the yaml configuration file")
+    args = parser.parse_args()
+    config = read_yaml(args.filename)
+    for level_type, levels in config["pedal_assist_levels"].items():
+        for index, level in enumerate(levels["levels"]):
+            # Header example ASSIST_LEVEL_STANDARD_0_FLAGS
+            for level_key, level_value in level.items():
+                print(format_header_define(f"ASSIST_LEVEL_{level_type}_{index}_{level_key}".upper(), level_value))
+            print()
+    del config["pedal_assist_levels"]
+    recurse_config_key(config)
 
 
 if __name__ == "__main__":
---
+++ src/programmer/validation.py
@@ -0,0 +1,75 @@
+from typing import Annotated, Literal
+
+from pydantic import BaseModel, Field
+from pydantic_pint import PydanticPintQuantity
+from pint import Quantity
+
+class BatterySocOffsetPercent(BaseModel):
+    empty: int = Field(ge=0, le=100, default=8)
+    full: int = Field(ge=0, le=100, default=8)
+
+class Battery(BaseModel):
+    nominal_voltage: Annotated[Quantity, PydanticPintQuantity("volt")]
+    minimum_voltage: Annotated[Quantity, PydanticPintQuantity("volt"), Field(default="36V")]
+    voltage_calibration_offset: Annotated[Quantity, PydanticPintQuantity("volt"), Field(default="0V")]
+    soc_offset_percent: BatterySocOffsetPercent
+    no_load_delay: Annotated[Quantity, PydanticPintQuantity("second"), Field(default="2s")]
+    low_voltage_ramp_down_percent: int = Field(ge=0, le=100, default=10)
+    low_voltage_current_percent: int = Field(ge=0, le=100, default=20)
+    soc_map: Literal["none", "SW102"] = Field(default="none")
+
+class SpeedSensor(BaseModel):
+    enabled: bool = Field(default=True)
+    signals_per_revolution: int = Field(ge=0, default=1)
+
+class ShiftSensor(BaseModel):
+    enabled: bool = Field(default=True)
+    interrupt_duration: Annotated[Quantity, PydanticPintQuantity("millisecond"), Field(default="600ms")]
+    interrupt_current_percent: int = Field(ge=0, le=100, default=10)
+
+class TemperatureSensor(BaseModel):
+    enabled: bool = Field(default=True)
+    use_sensor: Literal["controller", "motor"]
+    max_temperature: Annotated[Quantity, PydanticPintQuantity("degree_celsius"), Field(default="85C")]
+    max_temperature_ramp_down_start: Annotated[Quantity, PydanticPintQuantity("degree_celsius"), Field(default="5C")]
+    max_temperature_low_current_percent: int = Field(ge=0, le=100, default=20)
+
+class WalkMode(BaseModel):
+    enabled: bool = Field(default=True)
+    speed: Annotated[Quantity, PydanticPintQuantity("km/h"), Field(default="4km/h")]
+
+class Pretension(BaseModel):
+    enabled: bool = Field(default=False)
+    speed_cutoff: Annotated[Quantity, PydanticPintQuantity("km/h"), Field(default="16km/h")]
+
+class Lights(BaseModel):
+    enabled: bool = Field(default=True)
+    mode: Literal["default", "always_on", "brake_light"]
+
+class PedalAssistLevel(BaseModel):
+	flags: str = Field(default="ASSIST_FLAG_PAS | ASSIST_FLAG_THROTTLE")
+	max_cadence_percent: int = Field(ge=0, le=100, default=0)
+	max_speed_percent: int = Field(ge=0, le=100, default=0)
+	max_throttle_current_percent: int = Field(ge=0, le=100, default=0)
+	target_current_percent: int = Field(ge=0, le=100, default=0)
+
+class PedalAssistLevelType(BaseModel):
+    levels: list[PedalAssistLevel]
+
+class PedalAssistLevels(BaseModel):
+	standard: PedalAssistLevelType
+	sport: PedalAssistLevelType
+
+class Config(BaseModel):
+    motor_type: Literal["BBSHD", "BBS02_750W", "BBS02_500W"]
+    max_current: Annotated[Quantity, PydanticPintQuantity("ampere")]
+    global_max_speed: Annotated[Quantity, PydanticPintQuantity("km/h")]
+    wheel_circumference_mm: Annotated[Quantity, PydanticPintQuantity("millimeter")]
+    battery: Battery
+    speed_sensor: SpeedSensor
+    shift_sensor: ShiftSensor
+    temperature_sensor: TemperatureSensor
+    walk_mode: WalkMode
+    pretension: Pretension
+    lights: Lights
+    pedal_assist_levels: PedalAssistLevels
